7a79527d3b190f94b7256816565f8ce4cf1b4369
Audrey Schmidt <audsch@outlook.com>
2022-01-12 10:40:27 -0600

    created Model Song with SongList views with get/post requests, mapped urls accordingly, created serializers for data

--- musiclibrary/music/models.py
+++ musiclibrary/music/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    artist = models.CharField(max_length=50, null=False)
+    album = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    release_date = models.DateField()
+    likes = models.IntegerField()
+   
