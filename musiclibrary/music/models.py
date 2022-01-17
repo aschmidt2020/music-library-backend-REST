bd5c0761227ed42bb368e3cfb1aa87f324969ea1
Audrey Schmidt <audsch@outlook.com>
2022-01-17 08:56:50 -0600

    added null True/False to all Song model fields

--- musiclibrary/music/models.py
+++ musiclibrary/music/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Song(models.Model):
     title = models.CharField(max_length=50, null=False)
     artist = models.CharField(max_length=50, null=False)
-    album = models.CharField(max_length=50)
-    album_art_link = models.TextField(max_length=None)
-    genre = models.CharField(max_length=50)
-    release_date = models.DateField()
-    likes = models.IntegerField()
+    album = models.CharField(max_length=50, null=True)
+    album_art_link = models.TextField(max_length=None, null=True)
+    genre = models.CharField(max_length=50, null=True)
+    release_date = models.DateField(null=True)
+    likes = models.IntegerField(null=True)
     
     def __str__(self):
         return self.title + ' ' + self.artist
